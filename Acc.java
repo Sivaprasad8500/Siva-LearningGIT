afc83220ab9c23218dfb0c2561794499d20201f8
Sivaprasad8500 <sivaprasadpapasani@gmail.com>
2022-10-17 19:22:34 +0800

    fristcommit

---
+++ Acc.java
@@ -0,0 +1,27 @@
+public class Account {
+
+    String accNo;
+    String accName;
+
+    public Account(String accNo, String accName) {
+        this.accNo = accNo;
+        this.accName = accName;
+        this.accType =  Bussiness;
+    }
+
+    public String getAccNo() {
+        return accNo;
+    }
+
+    public void setAccNo(String accNo) {
+        this.accNo = accNo;
+    }
+
+    public String getAccName() {
+        return accName;
+    }
+
+    public void setAccName(String accName) {
+        this.accName = accName;
+    }
+}
